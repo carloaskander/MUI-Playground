7afd42c35297bf446c536e73b12f2e4ac77f562b
Carlo Askander <carloaskander@live.se>
2023-10-02 18:58:55 +0200

    Added Avatars & List.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,81 @@
+// React Imports
 import React from 'react';
-import { Container, Grid, Skeleton, Button, Box, Typography, Avatar, List } from '@mui/material';
-import { pink, blue, orange } from '@mui/material/colors';
 
+// MUI Imports
+import { Container, Grid, Skeleton, Button, Box, Typography, Stack, Avatar, AvatarGroup, List, Badge, ListItem, ListItemButton, ListItemIcon } from '@mui/material';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import { styled } from '@mui/material/styles';
+import { pink, blue, orange, deepOrange } from '@mui/material/colors';
+
+// CSS
 import './Main.css';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: 'ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
+
+const SmallAvatar = styled(Avatar)(({ theme }) => ({
+  width: 22,
+  height: 22,
+  border: `2px solid ${theme.palette.background.paper}`,
+}));
+
+function stringToColor(string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  };
+}
 
 function Main() {
   return (
@@ -111,9 +184,88 @@ function Main() {
       </Container>
 
 
-      <Container className='avatar-section'>
+      <Container className='avatar-section' style={{ marginBottom: '4rem' }}>
         <Typography variant="h3" gutterBottom style={{ fontSize: '2rem' }}>Avatars</Typography>
 
+        <Stack direction="row" spacing={2}>
+          <Avatar>A</Avatar>
+          <Avatar>B</Avatar>
+          <Avatar>C</Avatar>
+
+          <Avatar sx={{ bgcolor: deepOrange[400], width: 24, height: 24 }}>S</Avatar>
+          <Avatar variant='rounded' sx={{ bgcolor: pink[400] }}>M</Avatar>
+          <Avatar variant='square' sx={{ bgcolor: blue[400], width: 56, height: 56 }}>L</Avatar>
+
+        </Stack>
+        <Stack direction='row' spacing={2}>
+
+          <StyledBadge overlap="circular" anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} variant="dot">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </StyledBadge>
+
+          <Badge
+          overlap="circular"
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          badgeContent={
+            <SmallAvatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          }
+        >
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+          </Badge>
+
+          <AvatarGroup total={24}>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+            <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+          </AvatarGroup>
+
+          <Avatar {...stringAvatar('Kent Dodds')} />
+          <Avatar {...stringAvatar('Jed Watson')} />
+          <Avatar {...stringAvatar('Tim Neutkens')} />
+        </Stack>
+      </Container>
+
+
+      <Container className='list-section' style={{ marginBottom: '4rem' }}>
+        <Typography variant="h3" gutterBottom style={{ fontSize: '2rem' }}>List</Typography>
+        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          <nav aria-label="main mailbox folders">
+              <List>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <InboxIcon/>
+                    </ListItemIcon>
+                    <ListItemText primary="Inbox" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <DraftsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Drafts" />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+          </nav>
+          <Divider />
+          <nav aria-label='secondary mailbox folders'>
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Trash" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-list">
+                  <ListItemText primary="Spam" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </nav>
+        </Box>
       </Container>
     </main>
   );
